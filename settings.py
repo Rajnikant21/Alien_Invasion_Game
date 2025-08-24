94e6f3c2c2a0083a6e75801c167a02ddd154e210
Rajnikant Joshi <rajnikantjoshi2101@gmail.com>
2025-08-24 22:14:03 +0530

    new update

--- settings.py
+++ settings.py
@@ -9,18 +9,18 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Ship settings
-        self.ship_speed = 0.5
+        self.ship_speed = 1
 
         # Bullet settings
-        self.bullet_speed = 1.0
-        self.bullet_width = 3
+        self.bullet_speed = 1.5
+        self.bullet_width = 6
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullet_allowed = 3
+        self.bullet_allowed = 5
 
         # alien Settings
         self.alien_speed = 1.0
-        self.fleet_drop_speed = 10
+        self.fleet_drop_speed = 3
         # fleet_direction of 1 represents right; -1 represents left
         self.fleet_direction = 1
 
