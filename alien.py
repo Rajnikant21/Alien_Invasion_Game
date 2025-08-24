63ef668141868442b19ddd35058256f8443a9bfb
Rajnikant Joshi <rajnikantjoshi2101@gmail.com>
2025-08-24 14:13:41 +0530

    new update

--- alien.py
+++ alien.py
@@ -11,7 +11,7 @@ class Alien(Sprite):
         super().__init__()
         self.screen = ai_game.screen
         """As i reused the code from "Ship" module commented unused code"""
-        # self.settings = ai_game.settings
+        self.settings = ai_game.settings
         # self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.
@@ -25,3 +25,14 @@ class Alien(Sprite):
 
         # Store a decimal value for the ship's horizontal position
         self.x = float(self.rect.x)
+
+    def update(self):
+        """Move the alien right or left. """
+        self.x += (self.settings.alien_speed * self.settings.fleet_direction)
+        self.rect.x = self.x
+
+    def check_edges(self):
+        """ Retuurn True if alien is at edge of the screen"""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
